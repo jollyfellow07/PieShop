faf4949c28a4fcd1764cf3b16b9b8fc25cdd882e
jollyfellow7 <simonecompagnone7@gmail.com>
2022-08-18 12:09:51 +0200

    inserita una nuova view

--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -1,21 +1,21 @@
 using PieShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-app.UseStaticFiles();
 
 if(app.Environment.IsDevelopment())//se l app e in uso effettivamente allora:
 {
 
 app.UseDeveloperExceptionPage(); //mi fa vedere gli errori che riscontro nelle pagine
 }
+
+app.UseStaticFiles();
 
 app.MapDefaultControllerRoute();//  questo metodo controlla tutte le nostre rotte delle view, cioe per assicurare ch epossiamo vedere le pagine
 
